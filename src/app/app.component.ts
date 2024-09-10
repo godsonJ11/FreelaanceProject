a3b182faa2cee2bde522b1de07c6d9307f9a6757
godsonJ11 <godsongodson2000@gmail.com>
2024-09-10 18:58:47 +0530

    made some changes in register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {IndexComponent} from "./index/index.component";
 import {LoginComponent} from "./login/login.component";
 import { RegisterComponent } from './register/register.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RegisterOneComponent } from './register-one/register-one.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IndexComponent,LoginComponent,RegisterComponent],
+  imports: [RouterOutlet, IndexComponent,LoginComponent,RegisterComponent,RegisterOneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  logo:string='https://gallantize-organizer.s3.ap-south-1.amazonaws.com/gallantize.png';
+customValidator() {
+
+}
+onSaveUser() {
+console.log(this.register.value);
+}
+
+logo:string='https://gallantize-organizer.s3.ap-south-1.amazonaws.com/gallantize.png';
+
+  disable:boolean=false;
+  register:FormGroup;
+
+  constructor(private builder:FormBuilder){
+    this.register=this.builder.group({
+      clientName:new FormControl('',[Validators.required,Validators.minLength(3)]),
+      clientEmail:new FormControl('',[Validators.email,Validators.required]),
+      clientPassword:new FormControl('',[Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&*]).{8,}$')]),
+      clientCPassword:new FormControl('',[Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&*]).{8,}$')]),
+      clientBusinessName:new FormControl('',[Validators.required]),
+      clientBusinessType:new FormControl('',[Validators.required]),
+      clientPincode:new FormControl('',[Validators.required]),
+      clientCity:new FormControl('',[Validators.required])
+    })
+  }
+onChange(){
+  console.log(this.register.get('clientEmail')?.value);
+}
+
 }
